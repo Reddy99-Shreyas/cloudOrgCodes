c1de2773105e8a2236353fc1a4dca3c13383b101
Shreyas <129866308+Reddy99-Shreyas@users.noreply.github.com>
2023-12-07 23:11:53 +0530

    commit

--- force-app/main/default/lwc/demoCmp/demoCmp.js
+++ force-app/main/default/lwc/demoCmp/demoCmp.js
@@ -1,83 +1,123 @@
 import { LightningElement, track, api } from 'lwc';
 
-const salesforceStackOptions = [
-    { label: 'Sales Cloud', value: 'Sales Cloud', },
-    { label: 'Experience Cloud', value: 'Experience Cloud' },
-    { label: 'Service Cloud', value: 'Service Cloud' },
-    { label: 'App Cloud', value: 'App Cloud' },
-    { label: 'Community Cloud', value: 'Community Cloud' },
-    { label: 'LWC', value: 'LWC' },
-    { label: 'Apex', value: 'Apex' },
-    { label: 'Aura', value: 'Aura' }
-];
-const oracleStackOptions = [
-    { label: 'Java', value: 'Java' },
-    { label: 'ERP', value: 'ERP' },
-    { label: 'SQL', value: 'SQL' },
-    { label: 'MYSQL', value: 'MYSQL' },
-];
-const sapStackOptions = [
-    { label: 'SAP', value: 'SAP' },
-    { label: 'HANA', value: 'HANA' },
-    { label: 'ABAP', value: 'ABAP' },
-    { label: 'Blockchain', value: 'Blockchain' },
-];
-
 export default class DemoCmp extends LightningElement {
-    @track technologyOptions = [
-        { label: 'SAP', value: 'SAP' },
-        { label: 'Oracle', value: 'Oracle' },
-        { label: 'Salesforce', value: 'Salesforce' }
-    ];
+    @track showSalesforceStack = false;
+    @track showOracleStack = false;
+    @track showSAPStack = false;
 
-    @track technicalStackOptions = [];
+    value
+    get options() {
+        return [
+            { label: 'Salesforce', value: 'Salesforce' },
+            { label: 'Oracle', value: 'Oracle' },
+            { label: 'SAP', value: 'SAP' },
+        ];
+    }
 
-    @track showTechnicalStack = false;
-    @api selectedTechnicalStack = [];
+    handleChange(event) {
+        this.value = event.detail.value;
+        const selectedValue = event.detail.value;
 
-    handleTechnologyChange(event) {
-        const selectedTech = event.detail.value;
+        this.showSalesforceStack = selectedValue.includes('Salesforce');
+        this.showOracleStack = selectedValue.includes('Oracle');
+        this.showSAPStack = selectedValue.includes('SAP');
+    }
 
-        // Set options for technical stack based on selected technology
-        if (selectedTech === 'Salesforce') {
-            this.technicalStackOptions = salesforceStackOptions;
-        } else if (selectedTech === 'Oracle') {
-            this.technicalStackOptions = oracleStackOptions;
-        } else if (selectedTech === 'SAP') {
-            this.technicalStackOptions = sapStackOptions;
-        } else {
-            this.technicalStackOptions = [];
+    @api salesforcePicklistInput = ["Sales Cloud", "Service Cloud", "Marketing Cloud", "Commerce Cloud", "App Cloud", "Einstein Analytics", "Community Cloud", "IOTCloud", "Force.com", "Apex", "LWC", "Aura", "Salesforce"];
+    @api sapPicklistInput = ["ABAP", "SAP", "Hana"];
+
+    @api selectedItems = [];
+
+    @track allValues = []; // this will store end result or selected values from picklist
+    @track valuesVal = undefined;
+    @track searchTerm = '';
+    @track itemcounts = 'None Selected';
+
+    handleSearch(event) {
+        this.searchTerm = event.target.value;
+    }
+
+    get filteredResults() {
+        //copying data from parent component to local variables
+        if (this.valuesVal == undefined) {
+            this.valuesVal = this.salesforcePicklistInput;
+            //below method is used to change the input which we received from parent component
+            //we need input in array form, but if it's coming in JSON Object format, then we can use below piece of code to convert object to array
+            Object.keys(this.valuesVal).map(profile => {
+                this.allValues.push({ Id: profile, Name: this.valuesVal[profile] });
+            })
+
+            this.valuesVal = this.allValues.sort(function (a, b) { return a.Id - b.Id });
+            this.allValues = [];
+
+            console.log('da ', JSON.stringify(this.valuesVal));
         }
 
-        this.showTechnicalStack = this.technicalStackOptions.length > 0;
-        this.selectedTechnicalStack = [];
+        if (this.valuesVal != null && this.valuesVal.length != 0) {
+            if (this.valuesVal) {
+                const selectedProfileNames = this.selectedItems.map(profile => profile.Name);
+                console.log('selectedProfileNames ', JSON.stringify(selectedProfileNames));
+                return this.valuesVal.map(profile => {
+
+                    //below logic is used to show check mark (✓) in dropdown checklist
+                    const isChecked = selectedProfileNames.includes(profile.Id);
+                    return {
+                        ...profile,
+                        isChecked
+                    };
+
+                }).filter(profile =>
+                    profile.Id.toLowerCase().includes(this.searchTerm.toLowerCase())
+                ).slice(0, 20);
+            } else {
+                return [];
+            }
+        }
+    }
+    
+    handleSelection(event) {
+        const selectedProfileId = event.target.value;
+        const isChecked = event.target.checked;
+
+        //below logic is used to show check mark (✓) in dropdown checklist
+        if (isChecked) {
+            const selectedProfile = this.valuesVal.find(profile => profile.Id === selectedProfileId);
+            if (selectedProfile) {
+                this.selectedItems = [...this.selectedItems, selectedProfile];
+                this.allValues.push(selectedProfileId);
+            }
+        } else {
+            this.selectedItems = this.selectedItems.filter(profile => profile.Id !== selectedProfileId);
+            this.allValues.splice(this.allValues.indexOf(selectedProfileId), 1);
+        }
+        this.itemcounts = this.selectedItems.length > 0 ? `${this.selectedItems.length} options selected` : 'None Selected';
     }
 
-    handleTechnicalStackChange(event) {
-        this.selectedTechnicalStack = event.detail.value;
-        console.log(JSON.stringify(this.selectedTechnicalStack));
+    //Remove function for lightning pill
+    handleRemove(event) {
+        const valueRemoved = event.target.name;
+        this.selectedItems = this.selectedItems.filter(profile => profile.Id !== valueRemoved);
+        this.allValues.splice(this.allValues.indexOf(valueRemoved), 1);
+        this.itemcounts = this.selectedItems.length > 0 ? `${this.selectedItems.length} options selected` : 'None Selected';
     }
 
-    selectAll(event) {
-        this.selectedTechnicalStack = [...this.technicalStackOptions.map(option => option.value)];
-        console.log(JSON.stringify(this.selectedTechnicalStack));
+    selectall(event) {
+        event.preventDefault();
+
+        this.selectedItems = this.valuesVal;
+        this.itemcounts = this.selectedItems.length + ' options selected';
+        this.allValues = [];
     }
 
-    clearAll() {
-        this.selectedTechnicalStack = [];
-        console.log(JSON.stringify(this.selectedTechnicalStack));
+    handleclearall(event) {
+        event.preventDefault();
+
+        this.selectedItems = [];
+        this.allValues = [];
+        this.itemcounts = 'None Selected';
+        this.searchTerm = '';
     }
 
-    handlePillRemove(event) {
-        const pillIndex = event.detail.index ? event.detail.index : event.detail.name;
-        //console.log("Pill Index;=>" +pillIndex);
-        const itempill = this.selectedTechnicalStack;
-        itempill.splice(pillIndex, 1);       
-        this.selectedTechnicalStack = [...itempill];
-        console.log(JSON.stringify(this.selectedTechnicalStack));
-    }
-
-    // get selectedValues() {
-    //     return this.selectedTechnicalStack.join(';');
-    // }
+    //oracle logic
+    @api oraclePicklistInput = ["Java", "SQL", "ERP", "DBA"];
 }
